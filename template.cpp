7c953fbc14b5c6f8c95f6f94f175b492c738f492
pedroagaponto <pedrohmpereira@gmail.com>
2017-04-25 16:07:18 -0300

    Simple C++ template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+#ifdef LOCALTEST
+#define DEBUG(x) cout << '>' << #x << ':' << x << endl;
+#else
+#define DEBUG(x)
+#endif
+
+int main()
+{
+
+	return 0;
+}
+
